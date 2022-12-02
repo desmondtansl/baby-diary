c55bafbcbb8f313daf4d651113a736685f43b35f
Desmond Tan <desmondtansl90@gmail.com>
2022-12-02 09:35:36 +0800

    slight tweaks to text

--- src/Pages/RecipeSearch.jsx
+++ src/Pages/RecipeSearch.jsx
@@ -35,9 +35,9 @@ function RecipeSearch ({choice}) {
     return (
         <div className=" bg-orange-300 h-auto min-h-screen">
             <form onSubmit={handleSubmit}>
-            <h1>{choice} intolerance set</h1>
-            <div className=" pb-2 text-2xl" htmlFor="Recipe Search">Recipe Search</div>
-            <input type="text" placeholder="Results excl intolerances" onChange={handleChange}/>
+            <h1>Intolerance: {choice}</h1>
+            <div className=" pb-2 text-2xl" htmlFor="Recipe Search">Search for suitable recipes:</div>
+            <input type="text" onChange={handleChange}/>
             <button 
             className=" text-base p-3 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" 
             type="Search">
